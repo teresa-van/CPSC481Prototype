a57472e37fc9e60832950ad8a6d674b078c4e41c
Thomas Vu <thomas.vu@ucalgary.ca>
2018-12-09 18:22:50 -0700

    Added second page and profile folder and started context menu

--- CPSC481Prototype/MainWindow.xaml.cs
+++ CPSC481Prototype/MainWindow.xaml.cs
@@ -23,6 +23,73 @@ namespace CPSC481Prototype
 		public MainWindow()
 		{
 			InitializeComponent();
-		}
-	}
+            MainGrid.Children[3].Visibility = Visibility.Hidden;
+            MainGrid.Children[4].Visibility = Visibility.Hidden;
+            MainGrid.Children[5].Visibility = Visibility.Hidden;
+        }
+
+        private void HandleMouseWheel(MouseWheelEventArgs e)
+        {
+            if (e.Delta > 0)
+            {
+                MainGrid.Children[2].Visibility = Visibility.Visible;
+                MainGrid.Children[3].Visibility = Visibility.Hidden;
+            }
+            else if (e.Delta < 0)
+            {
+                MainGrid.Children[2].Visibility = Visibility.Hidden;
+                MainGrid.Children[3].Visibility = Visibility.Visible;
+            }
+        }
+
+        private void Homepage_BG_MouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            HandleMouseWheel(e);
+        }
+
+        private void Dock_MouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            HandleMouseWheel(e);
+        }
+
+        private void Homepage_Page1_MouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            HandleMouseWheel(e);
+        }
+
+        private void Homepage_Page2_MouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            HandleMouseWheel(e);
+        }
+
+        bool profile_open = false;
+        private void ___Button__Profile2_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (profile_open)
+            {
+                MainGrid.Children[4].Visibility = Visibility.Hidden;
+                profile_open = false;
+            }
+            else
+            {
+                MainGrid.Children[4].Visibility = Visibility.Visible;
+                profile_open = true;
+            }           
+        }
+        bool contextmenu_open = false;
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.D1)
+                if (contextmenu_open)
+                {
+                    MainGrid.Children[5].Visibility = Visibility.Hidden;
+                    contextmenu_open = false;
+                }
+                else
+                {
+                    MainGrid.Children[5].Visibility = Visibility.Visible;
+                    contextmenu_open = true;
+                }
+        }
+    }
 }
