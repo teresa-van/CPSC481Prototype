0ee8e2b5aa619b12dd9c888c1040ec44305d3a09
christo4 <christopher.rodrigue@ucalgary.ca>
2018-12-09 22:42:22 -0700

    Add files via upload

---
+++ CPSC481Prototype-master/CPSC481Prototype/MainWindow.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CPSC481Prototype
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+            Homepage_Page2.Visibility = Visibility.Hidden;
+            ProfileFolder.Visibility = Visibility.Hidden;
+            ContextMenu.Visibility = Visibility.Hidden;
+            StoreGrid.Visibility = Visibility.Hidden;
+        }
+
+        private void Homepage_MouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            if (e.Delta > 0)
+            {
+                Homepage_Page1.Visibility = Visibility.Visible;
+                Homepage_Page2.Visibility = Visibility.Hidden;
+            }
+            else if (e.Delta < 0)
+            {
+                Homepage_Page1.Visibility = Visibility.Hidden;
+                Homepage_Page2.Visibility = Visibility.Visible;
+            }
+        }
+
+        bool profile_open = false;
+        private void ___Button__Profile2_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (profile_open)
+            {
+                ProfileFolder.Visibility = Visibility.Hidden;
+                profile_open = false;
+            }
+            else
+            {
+                ProfileFolder.Visibility = Visibility.Visible;
+                profile_open = true;
+            }           
+        }
+
+        bool contextmenu_open = false;
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.D1)
+                if (contextmenu_open)
+                {
+                    ContextMenu.Visibility = Visibility.Hidden;
+                    contextmenu_open = false;
+                }
+                else
+                {
+                    ContextMenu.Visibility = Visibility.Visible;
+                    contextmenu_open = true;
+                }
+        }
+        bool storegrid_open = false;
+        private void ___Button__Store_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (storegrid_open)
+            {
+                StoreGrid.Visibility = Visibility.Hidden;
+                storegrid_open = false;
+            }
+            else
+            {
+                StoreGrid.Visibility = Visibility.Visible;
+                storegrid_open = true;
+            }
+        }
+    }
+}
